185a5109c74e54a2a30309aba7bb5f6d0abf1614
John Doe <aditisingh07071994@gmail.com>
2026-01-29 08:19:39 +0530

    initial4

--- app/actions/adminTechnicianActions.ts
+++ app/actions/adminTechnicianActions.ts
@@ -2,7 +2,7 @@
 
 import prisma from '@/lib/prisma';
 import { revalidatePath } from 'next/cache';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 // --- 1. GET TECHNICIANS LIST ---
 export async function getTechnicians() {
